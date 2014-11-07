384f1b3c88b40eadda34e62f1dbe5023509d083e
Daniel Choi <dhchoi@gmail.com>
2014-11-07 08:22:38 -0500

    add sample tests

--- Ng/Tests.hs
+++ Ng/Tests.hs
@@ -1,12 +1,17 @@
+{-# LANGUAGE QuasiQuotes, OverloadedStrings #-}
 module Ng.Tests where
 import Test.HUnit 
+import Ng.Expressions
+import Data.Aeson
+import Data.String.QQ
 
 
 runTests = runTestTT tests
 
-test1 = TestCase (assertEqual "eq (1,2)," (1,2) (1,2))
-
-tests = TestList [TestLabel "test1" test1]
+tests = test [
+    "tests1" ~: "eq (1,2)" ~: (1,2) @=? (1,1+1)
+  , "tests2" ~: "eq (2,2)" ~: (2,2) @=? (1,1+1)  -- fails
+             ]
 
 
 
