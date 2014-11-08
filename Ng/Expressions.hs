b32f3b7457a9b0d79c6dde3313ce86885c4de387
Daniel Choi <dhchoi@gmail.com>
2014-11-08 06:40:57 -0500

    comparison of values passing tests

--- Ng/Expressions.hs
+++ Ng/Expressions.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings, QuasiQuotes, ExistentialQuantification #-}
 module Ng.Expressions where
 import Text.Parsec hiding (many, (<|>))
 import Data.Maybe (fromJust, fromMaybe)
@@ -25,6 +25,7 @@ data NgExpr = NgKeyPath [JSKey]
             | And NgExpr NgExpr
             | Neg NgExpr 
             | Compare String NgExpr NgExpr
+            | NgLiteral Value
       deriving (Show, Eq)
 
 data JSKey = ObjectKey Text | ArrayIndex Int  | Method Text
@@ -33,6 +34,10 @@ data JSKey = ObjectKey Text | ArrayIndex Int  | Method Text
 data TextChunk = PassThrough String | Interpolation String 
     deriving (Show, Eq)
 
+data ComparableValue = ComparableNumberValue Scientific
+                     | ComparableStringValue Text
+                     | ComparableBoolValue Bool
+    deriving (Ord, Eq, Show)
 
 symbol s = spaces *> string s <* spaces
 
@@ -47,19 +52,26 @@ ngExpr = do
      <|> try (do op <- comparisonOp; expr2 <- ngExpr; return $ Compare op expr1 expr2) 
      <|> return expr1
 
-comparisonOp = choice $ map symbol [">", "<", "==", ">=", "<=", "!="]
+comparisonOp = choice $ map (try . symbol) [">", "<", "==", ">=", "<=", "!="]
 
-ngExprTerm = (char '(' *> ngExpr <* char ')') <|> ngKeyPath'
-
-ngKeyPath' = NgKeyPath <$> ngKeyPath 
+ngExprTerm = (char '(' *> ngExpr <* char ')') <|>  ngLiteral <|> ngKeyPath
 
 ngKeyPath = do
     ks <- sepBy1 ((ArrayIndex . read <$> many1 digit <* char ']') <|> (toJSKey . T.pack <$> ngVarName))
                  (char '.' <|> char '[') 
-    return ks 
+    return $ NgKeyPath ks 
 
 ngVarName = many1 (alphaNum <|> char '$' <|> char '_')
 
+ngLiteral = NgLiteral <$> (ngNumber <|> ngString <|> ngBool)
+
+-- just integers for now
+ngNumber = Number . read <$> many1 digit  
+
+-- dumb simple implementation that does not deal with escaping
+ngString = String . T.pack <$> ((char '"' *> many (noneOf "\"") <* char '"') <|> (char '\'' *> many (noneOf "'") <* char '\''))
+
+ngBool = Bool <$> ((try (string "true") *> pure True) <|> (try (string "false") *> pure False))
 
 ------------------------------------------------------------------------
 
@@ -89,6 +101,7 @@ ngEval :: [Text] -> Value -> Value
 ngEval keyPath context = ngExprEval (NgKeyPath $ map toJSKey keyPath) context
 
 ngExprEval :: NgExpr -> Value -> Value
+ngExprEval (NgLiteral x) _ = x
 ngExprEval (NgKeyPath ks) v = ngEvaluate ks v
 ngExprEval (Or x y) v       = 
       let vx = ngExprEval x v 
@@ -104,8 +117,8 @@ ngExprEval (Neg x) v        =
           String "" -> Bool True   -- ? is this right?
           _ -> Bool False
 ngExprEval (Compare op x y) v      = 
-      let vx = valueToNum $ ngExprEval x v
-          vy = valueToNum $ ngExprEval y v
+      let vx = comparableValue $ ngExprEval x v
+          vy = comparableValue $ ngExprEval y v
       in case op of   
             ">" -> Bool $ vx > vy
             "<" -> Bool $ vx < vy
@@ -114,9 +127,13 @@ ngExprEval (Compare op x y) v      =
             "==" -> Bool $ vx == vy
             "!=" -> Bool $ vx /= vy
 
-valueToNum :: RealFloat a => Value -> a
-valueToNum (Number x) = toRealFloat x
-valueToNum _ = 0      -- PUNT
+
+comparableValue :: Value -> ComparableValue
+comparableValue (Number x) = ComparableNumberValue x
+comparableValue (String x) = ComparableStringValue x
+comparableValue (Bool x) = ComparableBoolValue x
+comparableValue x = error $ "can't make comparable value for " ++ show x
+
 
 -- evaluates the a JS key path against a Value context to a leaf Value
 ngEvaluate :: [JSKey] -> Value -> Value
@@ -129,7 +146,6 @@ ngEvaluate [] x@(Array _) = x
 ngEvaluate ((ObjectKey key):(Method "length"):[]) (Object s) = 
     case (HM.lookup key s) of
         (Just (Array vs)) -> toJSON $ V.length vs
-        -- x -> error $ "length lookup " ++ show key ++ " found: " ++ show x
         _ -> Null
 ngEvaluate ((ObjectKey key):xs) (Object s) = ngEvaluate xs (HM.lookupDefault Null key s)
 ngEvaluate ((ArrayIndex idx):xs) (Array v) = case V.length v > 0 of
@@ -166,7 +182,9 @@ parseText :: String -> [TextChunk]
 parseText = runParse (many ngTextChunk) 
 
 parseKeyExpr :: String -> [JSKey]
-parseKeyExpr = runParse ngKeyPath
+parseKeyExpr s = 
+      let (NgKeyPath ks) = runParse ngKeyPath s
+      in ks
 
 ngTextChunk :: ParsecT String () Identity TextChunk
 ngTextChunk = interpolationChunk <|> passThroughChunk
@@ -220,14 +238,25 @@ tests = test [
   , "parse ngexpr 3"        ~: (Or (NgKeyPath [ObjectKey "test"]) (NgKeyPath [ObjectKey "test2"]))
                                @=? runParse ngExpr "(test || test2)"
   , "parse ngexpr 4"        ~:  
-      And (Or (NgKeyPath [ObjectKey "test1"]) (NgKeyPath [ObjectKey "test2"])) (NgKeyPath [ObjectKey "test3"])
-      @=? runParse ngExpr "(test1 || test2) && test3"
+        And (Or (NgKeyPath [ObjectKey "test1"]) (NgKeyPath [ObjectKey "test2"])) (NgKeyPath [ObjectKey "test3"])
+        @=? runParse ngExpr "(test1 || test2) && test3"
+  , "parse literal"         ~: NgLiteral (Number 2)  @=? runParse ngExpr "2"
   , "parse negation"        ~: Neg (NgKeyPath [ObjectKey "test"]) @=? runParse ngExpr "!test"
   , "parse comparison"      ~: Compare ">" (NgKeyPath [ObjectKey "test"]) (NgKeyPath [ObjectKey "test2"])
                                @=? runParse ngExpr "test > test2"
-  , "disjunction left"      ~: "apple"              @=?   ngEvalToString testContext1 "item || another" 
-  , "disjunction right"     ~: "10"                 @=?   ngEvalToString testContext1 "blah || another" 
-  , "disjunction in parens" ~: "apple"              @=?   ngEvalToString testContext2 "(item.color || item.name)" 
+  , "parse comparison with literal" ~: 
+                               Compare "==" (NgKeyPath [ObjectKey "test"]) (NgLiteral (Number 1))
+                               @=? runParse ngExpr "test == 1"
+  , "disjunction left"      ~: "apple"               @=? ngEvalToString testContext1 "item || another" 
+  , "disjunction right"     ~: "10"                  @=? ngEvalToString testContext1 "blah || another" 
+  , "disjunction in parens" ~: "apple"               @=? ngEvalToString testContext2 "(item.color || item.name)" 
+  , "length"                ~: Number 3                   @=? ngEval ["items","length"] testContext3 
+  , "compare 1"             ~: Bool False
+                               @=? ngExprEval (runParse ngExpr "items.length == 2") testContext3
+  , "compare 2"             ~: Bool True
+                               @=? ngExprEval (runParse ngExpr "items.length == 3") testContext3
+  , "compare to string"     ~: "true"
+                               @=? ngEvalToString testContext1 "item == 'apple'"
   , "text chunk 1"          ~: Interpolation "test"  @=? runParse interpolationChunk "{{test}}"
   , "text chunk 2"          ~: PassThrough "test"    @=? runParse passThroughChunk "test"
   , "text chunk 3"          ~: PassThrough " test"   @=? runParse ngTextChunk " test"
